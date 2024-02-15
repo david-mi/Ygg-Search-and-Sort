f0725a27b7cf849927b7d847621620c46bd4056c
David Michel <david.michel2809@gmail.com>
2024-02-15 14:19:48 +0100

    refacto: separate code into modules

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,14 @@
-interface SortParams {
-  order: "asc" | "desc",
-  sortBy: "seed"
-}
+import {
+  setSearchParamsFromFormData,
+  redirect,
+  setSortParams,
+  getSearchForms
+} from "./helpers";
 
 (function () {
   console.log("YGG - Auto sort by seed v1.2.0");
 
-  function searchFormsSelector() {
-    return document.querySelectorAll<HTMLFormElement>(`form[action='https://${formatHostname()}/engine/search']`)
-  }
-
-  function formatHostname() {
-    const { hostname } = location
-    return `www3.${hostname.replace(/.+(?=ygg)/, "")}`
-  }
-
-  const searchForms = searchFormsSelector()
+  const searchForms = getSearchForms()
 
   if (searchForms.length === 0) {
     return console.error({ YggCustomScriptError: "Aucun formulaire de recherche trouvé" })
@@ -32,23 +25,6 @@ interface SortParams {
       sortParams: { order: "desc", sortBy: "seed" }
     })
     redirect(actionUrl)
-  }
-
-  function setSearchParamsFromFormData({ formData, url }: { formData: FormData, url: URL }) {
-    for (const [name, value] of formData.entries()) {
-      url.searchParams.set(name, value as string)
-    }
-
-    url.searchParams.set("do", "search")
-  }
-
-  function setSortParams({ url, sortParams }: { url: URL, sortParams: SortParams }) {
-    url.searchParams.set("order", sortParams.order)
-    url.searchParams.set("sort", sortParams.sortBy)
-  }
-
-  function redirect(url: URL) {
-    window.location.replace(url)
   }
 
   searchForms.forEach((searchForm) => searchForm.addEventListener("submit", handleFormSubmit))
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface SortParams {
+  order: "asc" | "desc",
+  sortBy: "seed"
+}
