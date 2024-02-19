7cd30424464368f214c04743c7a0550f45954f44
David Michel <david.michel2809@gmail.com>
2024-02-19 15:41:27 +0100

    refacto: simplify orderButtonClick files by reusing setSortFormElementsAttributes

--- src/handlers/sortForm/orderButtonClick.ts
+++ src/handlers/sortForm/orderButtonClick.ts
@@ -1,28 +1,15 @@
 import { Store } from "@controllers/store"
 import type { SortOrder } from "@types"
 import { SORT_BUTTON_DATA_ATTRIBUTES } from "@views/constants"
-
-function handleNonClickedOrderButton(clickedOrderButton: HTMLButtonElement) {
-  const clickedOrderButtonParentElement = clickedOrderButton.parentElement as HTMLDivElement
-  const currentActiveOrderButton = clickedOrderButtonParentElement.querySelector(`[${SORT_BUTTON_DATA_ATTRIBUTES.ACTIVE}]`)
-
-  if (currentActiveOrderButton) {
-    currentActiveOrderButton.removeAttribute(SORT_BUTTON_DATA_ATTRIBUTES.ACTIVE)
-  }
-}
-
-function handleButtonsActiveAttribute(targetButton: HTMLButtonElement) {
-  handleNonClickedOrderButton(targetButton)
-  targetButton.setAttribute(SORT_BUTTON_DATA_ATTRIBUTES.ACTIVE, "")
-}
+import { setSortFormElementsAttributes } from "@views/setAttributes"
 
 export function handleOrderButtonClick(event: MouseEvent) {
   const orderButton = event.target as HTMLButtonElement
-
-  handleButtonsActiveAttribute(orderButton)
 
   Store.setData("sortParams", {
     type: Store.sortParams!.type,
     order: orderButton.getAttribute(SORT_BUTTON_DATA_ATTRIBUTES.ORDER) as SortOrder
   })
+
+  setSortFormElementsAttributes()
 }
