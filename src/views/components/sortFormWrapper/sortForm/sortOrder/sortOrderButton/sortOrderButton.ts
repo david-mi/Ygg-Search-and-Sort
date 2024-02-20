44489eab1d6558c2afb08af1c1746a4a5b4db597
David Michel <david.michel2809@gmail.com>
2024-02-20 10:32:32 +0100

    refacto/style: implement createCustomButtonWithSvgGradient with createSortOrderButton, improve its style

--- src/views/components/sortFormWrapper/sortForm/sortOrder/sortOrderButton/sortOrderButton.ts
+++ src/views/components/sortFormWrapper/sortForm/sortOrder/sortOrderButton/sortOrderButton.ts
@@ -1,17 +1,16 @@
-import { createCustomElement } from "@views/utils/createCustomElement";
-import styles from "./sortOrderButton.module.css"
-import { createArrowSvgHtml } from "./arrowSvg/arrowSvg";
 import { SORT_BUTTON_DATA_ATTRIBUTES } from "@views/constants";
 import type { SortOrder } from "@types";
+import { createCustomButtonWithSvgGradient } from "@views/components/shared/createCustomButtonWithSvgGradient/createCustomButtonWithSvgGradient";
+import styles from "./sortOrderButton.module.css"
 
 export function createSortOrderButton(sortOrder: SortOrder) {
-  return createCustomElement({
-    tagName: "button",
+  return createCustomButtonWithSvgGradient(({
     attributes: [
       ["class", styles.sortOrderButton],
       ["type", "button"],
       [SORT_BUTTON_DATA_ATTRIBUTES.ORDER, sortOrder],
     ],
-    childrens: [createArrowSvgHtml(sortOrder)]
-  })
+    paths: [`<path fill="currentColor" d="M11 18V9.825L7.4 13.4L6 12l6-6l6 6l-1.4 1.4L13 9.825V18h-2Z"/>`],
+    svgClassName: `${styles.arrowSvg} ${styles[sortOrder]}`
+  }))
 }
