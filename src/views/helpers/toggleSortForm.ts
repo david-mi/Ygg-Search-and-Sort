df3f00951cadf32a996d38c1cf3e09fa094e4eb9
David Michel <david.michel2809@gmail.com>
2024-02-27 10:16:50 +0100

    fix: fix display style in toggleSortFormDisplayMode

--- src/views/helpers/toggleSortForm.ts
+++ src/views/helpers/toggleSortForm.ts
@@ -2,8 +2,8 @@ import { sortForm } from "@components/sortFormWrapper/sortForm/sortForm";
 
 export function toggleSortFormDisplayMode() {
   const currentSortFormDisplayValue = sortForm.style.display
-
-  if (currentSortFormDisplayValue === "flex") {
+  console.log(currentSortFormDisplayValue)
+  if (currentSortFormDisplayValue === "grid") {
     sortForm.style.display = "none"
     document.removeEventListener("click", toggleSortFormDisplayMode)
   } else {
